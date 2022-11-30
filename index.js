e18f281664be0464240b6116888a0c8e4603c8c8
Nikhitha899866 <118981698+Nikhitha899866@users.noreply.github.com>
2022-11-30 17:24:45 -0500

    Added Scroll to top button

---
+++ index.js
@@ -0,0 +1,16 @@
+let mybutton = document.getElementById("myBtn");
+
+console.log("myButton", mybutton);
+
+window.onscroll = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+};
+
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
